75c4220d53ffd5aeb820946e265380312665e477
Octavio Llanillo <llanillo4@hotmail.com>
2023-02-06 22:20:55 -0300

    Change goblin BP class

--- Source/Labyrinth/Obtacle/Goblin.h
+++ Source/Labyrinth/Obtacle/Goblin.h
@@ -4,23 +4,18 @@
 #include "GameFramework/Character.h"
 #include "Goblin.generated.h"
 
-UCLASS()
+UCLASS(ClassGroup=LAB)
 class LABYRINTH_API AGoblin : public ACharacter
 {
 	GENERATED_BODY()
 
-public:
-	// Sets default values for this pawn's properties
-	AGoblin();
-
 protected:
-	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:
-	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+public:
+	AGoblin();
 };
